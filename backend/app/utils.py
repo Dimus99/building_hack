45b41b9a526245049b1b62a6ad9847377b06da7d
Dimus99 <shelpyakovd@bk.ru>
2023-06-24 21:56:13 +0500

    bugs

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -17,6 +17,8 @@ def merge_with_attr(dataset):
         else:
             print(4)
             dataset['date_report'] = pd.to_datetime("2023.06.05")
+        print(4.5)
+        print("date_report" in attr)
         attr['date_report'] = pd.to_datetime(attr['date_report'])
         print(5)
 
