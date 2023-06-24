382d515c6d222fbd397fba2cc038da25bfd9c13a
Dimus99 <shelpyakovd@bk.ru>
2023-06-24 21:53:01 +0500

    columns

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -9,7 +9,7 @@ def merge_with_attr(dataset):
     try:
         attr = pd.read_csv(attr_path, delimiter=";", on_bad_lines="skip")
         dataset['date_report'] = pd.to_datetime(
-            dataset['date_report']) if "date_report" in dataset else pd.to_datetime("2023.06.05")
+            dataset['date_report']) if "date_report" in dataset.columns else pd.to_datetime("2023.06.05")
         attr['date_report'] = pd.to_datetime(attr['date_report'])
 
         return pd.merge_asof(dataset, attr, on="date_report", by="obj_key")
