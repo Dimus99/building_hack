589cca898e50945736671aa1fdafdac54f7faf0c
Dimus99 <shelpyakovd@bk.ru>
2023-06-24 14:55:28 +0500

    join csv

---
+++ research/join_tables.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+dataset_path = "./data/dataset_big_v2.csv"
+attr_path = "./data/attr_v2.csv"
+
+# Read the dataset and attribute files
+dataset = pd.read_csv(dataset_path, delimiter=";", on_bad_lines="skip")
+attr = pd.read_csv(attr_path, delimiter=";", on_bad_lines="skip")
+print(dataset.head())
+print(attr.head)
+dataset['date_report'] = pd.to_datetime(dataset['date_report'])
+attr['date_report'] = pd.to_datetime(attr['date_report'])
+
+
+merged_dataframe = pd.merge_asof(dataset, attr, on="date_report", by="obj_key")
+print(merged_dataframe.head())
+merged_dataframe.to_csv('joined.csv', index=False)
+
+
